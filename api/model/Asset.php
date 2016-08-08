e1903445a91c2fe6c1dc8e3fc96e694942a1a9ed
iceoton <iceoton@gmail.com>
2016-08-08 07:06:42 +0700

    define asset class variable

--- api/model/Asset.php
+++ api/model/Asset.php
@@ -2,5 +2,21 @@
 
 class Asset
 {
+    public $id;
+    public $code;
+    public $name;
+    public $detail;
+    public $category;
+    public $location;
+    public $source;
+    public $status;
+    public $unit;
+    public $quantity;
+    public $come_date;
+    public $update_date;
 
+    static function create(){
+        $instance = new self();
+        return $instance;
+    }
 }
