7d94561904f3435a6e59e061e01d0986fa1c83e8
iceoton <iceoton@gmail.com>
2017-10-06 09:19:59 +0700

    - add php info file

--- core/config.core.php
+++ core/config.core.php
@@ -2,15 +2,14 @@
 /** MySQL's config a constant value */
 
 // MySQL hostname
-//define('DB_HOST', '127.4.145.2');
+//define('DB_HOST', '128.199.222.110');
 define('DB_HOST', 'localhost');
 
 // MySQL database username
-//define('DB_USERNAME', 'adminfUMeDEG');
 define('DB_USERNAME', 'root');
 
 // MySQL database password
-//define('DB_PASSWORD', '6i9iEuF6xE3w');
+//define('DB_PASSWORD', '4cc29a5f63f397bfc73648a87de880b8a15df58e07426bb7');
 define('DB_PASSWORD', 'root');
 
 // MySQL database name
